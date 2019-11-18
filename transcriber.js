55a9634b30fed35ca0682d1e6f23cdfc67113f4d
Nic Hartley <nic@cybers.eco>
2019-11-18 14:01:02 -0500

    fix: additional missed letter

--- transcriber.js
+++ transcriber.js
@@ -1,11 +1,15 @@
 function replaceSubs(from) {
-  return from.
-    replace(/\\th/gi, "þ").
-    replace(/\\dh/gi, "ð").
-    replace(/\\ae/gi, "æ").
-    replace("Þ", "þ").
-    replace("Ð", "ð").
-    toLowerCase();
+  return from
+    .replace(/\\th/gi, "þ")
+    .replace(/\\dh/gi, "ð")
+    .replace(/\\ae/gi, "æ")
+    .replace(/\\ch/gi, "ȝ")
+    .replace("ƿ", "w")
+    .replace("Ƿ", "w")
+    .replace("Þ", "þ")
+    .replace("Ð", "ð")
+    .replace("Ȝ", "ȝ")
+    .toLowerCase();
 }
 
 function transcribe(from) {
