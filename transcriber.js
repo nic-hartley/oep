3b60ec49267459728e7d10b25fd2c071d1a17b8c
Nic Hartley <nic@cybers.eco>
2019-11-18 13:01:49 -0500

    feat: input normalization

--- transcriber.js
+++ transcriber.js
@@ -1,4 +1,14 @@
+function replaceSubs(from) {
+  return from.
+    replace("\\th", "þ").
+    replace("\\dh", "ð").
+    replace("Þ", "þ").
+    replace("Ð", "ð").
+    toLowerCase();
+}
+
 function transcribe(from) {
   // TODO
+  from = replaceSubs(from);
   return from;
 }
