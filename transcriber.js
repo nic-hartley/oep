c334494b7dbd552174ce29ebb8e6e75dbbb88faa
Nic Hartley <nic@cybers.eco>
2019-11-18 13:23:59 -0500

    fix: ignore case for escapes, add a missing one

--- transcriber.js
+++ transcriber.js
@@ -1,7 +1,8 @@
 function replaceSubs(from) {
   return from.
-    replace("\\th", "þ").
-    replace("\\dh", "ð").
+    replace(/\\th/i, "þ").
+    replace(/\\dh/i, "ð").
+    replace(/\\ae/i, "æ").
     replace("Þ", "þ").
     replace("Ð", "ð").
     toLowerCase();
